5598090533e0a908fc04677c3018fd151970028f
Tomcatdaboss <140665293+Tomcatdaboss@users.noreply.github.com>
2025-02-12 17:26:51 +1100

    Update Slot.cs

--- Slot.cs
+++ Slot.cs
@@ -8,6 +8,60 @@ public class Slot : MonoBehaviour
     public GameObject item_icon;
     public Sprite wood_icon_sprite;
     public Sprite stone_icon_sprite;
+    public Sprite rawmeat_icon_sprite;
+    public Sprite ore_icon_sprite;
+    public Sprite null_sprite;
+    public int quantity = 0;
+    public string item_name = "empty";
+    public bool isInvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (item_name == "empty"){
+            item_info.GetComponent<Text>().text = "Empty Slot";
+            item_icon.GetComponent<Image>().sprite = null_sprite;
+        } else {
+            if (isInvent){
+                item_info.GetComponent<Text>().text = item_name + " X " + quantity;
+            } else {
+                item_info.GetComponent<Text>().text = item_name;
+            }            
+            if(item_name == "Wood"){
+                item_icon.GetComponent<Image>().sprite = wood_icon_sprite;
+            }
+            if(item_name == "Stone"){
+                item_icon.GetComponent<Image>().sprite = stone_icon_sprite;
+            }
+            if(item_name == "Raw Meat"){
+                item_icon.GetComponent<Image>().sprite = rawmeat_icon_sprite;
+            }
+            if(item_name == "Ore"){
+                item_icon.GetComponent<Image>().sprite = ore_icon_sprite;
+            }
+        }
+        if (quantity == 0){
+            item_name = "empty";
+            item_info.GetComponent<Text>().text = "Empty Slot";
+            item_icon.GetComponent<Image>().sprite = null_sprite;
+        }
+    }
+}
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Slot : MonoBehaviour
+{
+    public GameObject item_info;
+    public GameObject item_icon;
+    public Sprite wood_icon_sprite;
+    public Sprite stone_icon_sprite;
     public Sprite fiber_icon_sprite;
     public Sprite null_sprite;
     public int quantity = 0;
