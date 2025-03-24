148b1d654ed4fa751d2272ea2cc479fdae88f9f8
Tomcatdaboss <140665293+Tomcatdaboss@users.noreply.github.com>
2025-03-24 15:27:50 +1100

    Update Slot.cs
    added comments describing the functions of the file

--- Slot.cs
+++ Slot.cs
@@ -17,29 +17,34 @@ public class Slot : MonoBehaviour
     public Sprite fibre_icon_sprite;
     public Sprite cookedmeat_icon_sprite;
     public Sprite rope_icon_sprite;
+    public Sprite hide_icon_sprite;
+    public Sprite net_icon_sprite;
+    public Sprite hull_icon_sprite;
+    public Sprite flag_icon_sprite;
+    public Sprite compass_icon_sprite;
+    public Sprite sail_icon_sprite;
     public Sprite null_sprite;
     public int quantity = 0;
     public string item_name = "empty";
     public bool isInvent;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if (item_name == "empty"){
+        if (item_name == "empty"){ // if there is no item in the inventory, then call it an Empty slot
+            item_info.GetComponent<Text>().text = "Empty Slot";
+            item_icon.GetComponent<Image>().sprite = null_sprite;
+        } else if (quantity == 0){
+            item_name = "empty";
             item_info.GetComponent<Text>().text = "Empty Slot";
             item_icon.GetComponent<Image>().sprite = null_sprite;
         } else {
-            if (isInvent){
+            if (isInvent){ // defines correct method of stating the item name and quantity depending on whether the slot is an equip or invent slot
                 item_info.GetComponent<Text>().text = item_name + " X " + quantity;
             } else {
                 item_info.GetComponent<Text>().text = item_name;
             }            
-            if(item_name == "Wood"){
+            if(item_name == "Wood"){ // essentially all these if statements do is check if the item in the slot is a specific type of item, and if so, change the item icon to the correct sprite.
                 item_icon.GetComponent<Image>().sprite = wood_icon_sprite;
             }
             if(item_name == "Stone"){
@@ -60,6 +65,9 @@ public class Slot : MonoBehaviour
             if(item_name == "Rope"){
                 item_icon.GetComponent<Image>().sprite = rope_icon_sprite;
             }
+            if(item_name == "Hide"){
+                item_icon.GetComponent<Image>().sprite = hide_icon_sprite;
+            }
             if(item_name == "Axe"){
                 item_icon.GetComponent<Image>().sprite = axe_icon_sprite;
             }
@@ -72,11 +80,21 @@ public class Slot : MonoBehaviour
             if(item_name == "Sickle"){
                 item_icon.GetComponent<Image>().sprite = sickle_icon_sprite;
             }
-        }
-        if (quantity == 0){
-            item_name = "empty";
-            item_info.GetComponent<Text>().text = "Empty Slot";
-            item_icon.GetComponent<Image>().sprite = null_sprite;
+            if(item_name == "Net"){
+                item_icon.GetComponent<Image>().sprite = net_icon_sprite;
+            }
+            if(item_name == "Hull"){
+                item_icon.GetComponent<Image>().sprite = hull_icon_sprite;
+            }
+            if(item_name == "Sail"){
+                item_icon.GetComponent<Image>().sprite = sail_icon_sprite;
+            }
+            if(item_name == "Flag"){
+                item_icon.GetComponent<Image>().sprite = flag_icon_sprite;
+            }
+            if(item_name == "Compass"){
+                item_icon.GetComponent<Image>().sprite = compass_icon_sprite;
+            }
         }
     }
 }
