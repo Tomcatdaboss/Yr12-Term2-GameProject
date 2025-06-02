3931760fad2fc419214167c1edcb2ef2fb32578a
Tomcatdaboss <140665293+Tomcatdaboss@users.noreply.github.com>
2025-06-02 13:06:28 +1000

    Update CameraMovementScript.cs
    final commit with commented code

--- CameraMovementScript.cs
+++ CameraMovementScript.cs
@@ -10,6 +10,7 @@ public class CameraMovementScript : MonoBehaviour
     public GameObject help_obj;
     public GameObject menubtnsprite;
     public GameObject campfireUI;
+    public GameObject cam_holder;
     private float xRotation;
     private float yRotation;
 
@@ -23,6 +24,7 @@ public class CameraMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.position = cam_holder.transform.position;
         // the if statement checks if inventory or the help screen is active, then if no, locks the cursor.
         if (inventory_sprite.activeSelf == false && help_obj.activeSelf == false && menusprite.activeSelf == false && menubtnsprite.activeSelf == false && campfireUI.activeSelf == false)
         { // testing that they are not in inventory
@@ -35,11 +37,15 @@ public class CameraMovementScript : MonoBehaviour
 
             yRotation += inputX;
             xRotation -= inputY;
+            // prevent player from looking too far up or down
             xRotation = Mathf.Clamp(xRotation, -90f, 50f);
+            // rotate camera & camera holder (arms and tools) in two axes, rotate player in one axis 
             transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
             player.transform.rotation = Quaternion.Euler(0, yRotation, 0);
+            cam_holder.transform.rotation = transform.rotation;
+
         }
-        else
+        else // if no menus are open hide the cursor
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
